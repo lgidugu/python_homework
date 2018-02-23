5d8a8f46ba354811636f6d7d45da20346051267e
lgidugu <32501780+lgidugu@users.noreply.github.com>
2018-02-22 23:51:13 -0500

    Week3 home work submitted

---
+++ Python_HW_WK3/pyPoll_hw/pyPoll1.py
@@ -0,0 +1,52 @@
+import os
+import csv
+import operator
+import collections
+
+
+print("Election Results")
+print("__________________________________")
+
+with open("election_data_1.csv") as pfile:
+    csv_reader = csv.reader(pfile, delimiter = ',')
+
+total_election_data_csv = os.path.join("election_data_1.csv")
+
+
+total_votes_cnt = 0
+with open(total_election_data_csv, newline ="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader, None)
+    for row in csvreader:
+       total_votes_cnt == row[0]
+       total_votes_cnt = total_votes_cnt +1
+    print("Total Votes are : " + str(total_votes_cnt))
+print("_________________________________")
+
+with open(total_election_data_csv, "r") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter =",")
+    next(csvreader, None)
+    list_o_candidates = []
+    for rows in csvreader:
+        list_o_candidates.append(rows[2])
+with open(total_election_data_csv, "r")as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    sort = sorted(list_o_candidates, key = operator.itemgetter(2))
+    counter = collections.Counter(list_o_candidates)
+
+    for (k, v) in counter.items():
+        percent_votes = round((int(v)/total_votes_cnt)*100, 1)
+        print(k, ":%s %%\n"% percent_votes,"(",v,")")
+    print("___________________________________")
+
+    inverse = [(value,key) for key, value in counter.items()]
+    print("Winner is  " + (max(inverse)[1]))
+
+
+    print("___________________________________")
+
+
+
+
+
+
---
+++ Python_HW_WK3/pyboss_hw/pyboss1.py
@@ -0,0 +1,57 @@
+import os
+import csv
+from us_state_abbrev import us_state_abbrev
+
+with open("employee_data1.csv") as pfile:
+    csv_reader = csv.reader(pfile, delimiter=',')
+# adding path to new file
+newBoss_data_csv = os.path.join("employee_data1.csv")
+
+#requied lists for new csv file format
+EmpID = []
+FirstName= []
+LastName = []
+DOB =[]
+SSN = []
+State =[]
+
+#opening and appending lists to new csv file in required formats
+with open(newBoss_data_csv, newline='') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=',')
+    next(csv_reader)
+    for row in csv_reader:
+        EmpID.append(row[0])
+        firstLastNames = row[1].split(' ')
+
+
+        FirstName.append(firstLastNames[0])
+        LastName.append(firstLastNames[1])
+
+
+        DOB.append(row[2])
+
+
+        newSSN_format = "***-**-" + row[3].split("-")[2]
+
+        SSN.append(newSSN_format)
+
+
+        stateName = us_state_abbrev[row[4]]
+        State.append(stateName)
+#zipping all the newlyformatted files
+cleanBoss_data_csv = zip(EmpID, FirstName, LastName, DOB, SSN, State)
+
+print(cleanBoss_data_csv)
+
+output_file = os.path.join("boss_final.csv")
+#writing to the new csvfile
+with open(output_file, "w",newline="") as datafile:
+    writer = csv.writer(datafile)
+
+    writer.writerow(['EmpID','FirstName','LastName','DOB','SSN','State'])
+    writer.writerows(cleanBoss_data_csv)
+
+with open('boss_final.csv') as csfile:
+    csv_reader = csv.reader(csfile, delimiter = ',')
+    for lines in csv_reader:
+        print(lines)
